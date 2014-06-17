f35329d0267152d4dd79bbd9e6d0b573d8b81620
Johnny Wu <KarasAya@gmail.com>
2014-06-17 16:54:25 +0800

    integrate atlas editor to workspace ctrl

--- src/js/paperUtils.js
+++ src/js/paperUtils.js
@@ -1,22 +1,25 @@
-var PaperUtils = {};
+var PaperUtils;
+(function (PaperUtils) {
 
-PaperUtils.createSpriteRaster = function (tex) {
-    var tmpRawRaster = new paper.Raster(tex.image);
-    var trimRect = new paper.Rectangle(tex.trimX, tex.trimY, tex.width, tex.height);
-    var raster = tmpRawRaster.getSubRaster(trimRect);
-    tmpRawRaster.remove();  // can only be removed after getSubRaster
-    raster.pivot = [-tex.width * 0.5, -tex.height * 0.5];
-    if (tex.rotated) {
-        raster.pivot = [raster.pivot.x, -raster.pivot.y];
-        raster.rotation = 90;
-    }
-    return raster;
-};
+    PaperUtils.createSpriteRaster = function (tex) {
+        var tmpRawRaster = new paper.Raster(tex.image);
+        var trimRect = new paper.Rectangle(tex.trimX, tex.trimY, tex.width, tex.height);
+        var raster = tmpRawRaster.getSubRaster(trimRect);
+        tmpRawRaster.remove();  // can only be removed after getSubRaster
+        raster.pivot = [-tex.width * 0.5, -tex.height * 0.5];
+        if (tex.rotated) {
+            raster.pivot = [raster.pivot.x, -raster.pivot.y];
+            raster.rotation = 90;
+        }
+        return raster;
+    };
 
-PaperUtils.createLayer = function () {
-    var newLayer = new paper.Layer(paper.Item.NO_INSERT);
-    newLayer.applyMatrix = false;
-    newLayer.position = [0, 0];   // in paper, position should be settled before pivot
-    newLayer.pivot = [0, 0];
-    return newLayer;
-};
+    PaperUtils.createLayer = function () {
+        var newLayer = new paper.Layer(paper.Item.NO_INSERT);
+        newLayer.applyMatrix = false;
+        newLayer.position = [0, 0];   // in paper, position should be settled before pivot
+        newLayer.pivot = [0, 0];
+        return newLayer;
+    };
+
+})(PaperUtils || (PaperUtils = {}));
